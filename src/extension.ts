e78c953652efc93fc5852adf3bffdfcaa0fab83a
SE2Dev <brokensilencedev@gmail.com>
2016-02-17 13:39:12 -0500

    Add CompletionItemProvider for Built-In Functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,12 @@
 'use strict';
 import * as vscode from 'vscode';
+import {completionItemProvider} from './features/completionItemProvider'
 
 export function activate(context: vscode.ExtensionContext)
 {
 	console.log('Init: "CoD-Sense"');
+	
+	vscode.languages.registerCompletionItemProvider("gsc", new completionItemProvider())
 }
 
 export function deactivate()
