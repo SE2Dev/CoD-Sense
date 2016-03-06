0626961d781293b3ba0fd77eff8ef041c9a74f08
SE2Dev <brokensilencedev@gmail.com>
2016-03-05 21:05:47 -0500

    Add Optional Logging of AST

--- debug/main.js
+++ debug/main.js
@@ -7,6 +7,7 @@ var ast;
 try {
     ast = parser.parse(source);
     console.log(ast);
+    //fs.writeFileSync("out.ast", JSON.stringify(ast), "utf8");
 }
 catch (exception) {
     console.log("Parse Error:  " + exception.message);
