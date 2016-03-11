8157bc1f646da225d3d81ec637d86d850ac574ce
SE2Dev <brokensilencedev@gmail.com>
2016-03-10 22:08:53 -0500

    Update Configuration Handler
    Disable Brute-Force Workspace Analysis

--- server/src/features/configurationHandler.ts
+++ server/src/features/configurationHandler.ts
@@ -1,21 +1,51 @@
 'use strict'
 import {console, connection} from "../server"
+
+import * as vss from "vscode-languageserver"
 import {    DidChangeConfigurationParams, 
             Location, Range, Position } from "vscode-languageserver"
 
 import {CoDSenseWorkspaceUrisRequest} from '../request'
 import * as ast from "../analyzer"
 
+import fs = require('fs');
+
+export interface ISettings
+{
+    libs: {};
+    active_lib: string; 
+}
+
+export class IConfiguration
+{
+    settings: ISettings;
+    workspacePath: string;
+    libraryPath: string;
+}
+
+export var config: IConfiguration = new IConfiguration;
+
 export function DidChangeConfigurationHandler(params: DidChangeConfigurationParams): void
 {
-    connection.sendRequest(CoDSenseWorkspaceUrisRequest.type, "gsc").then(
-        function(files) //Resolved
+    config.settings = params.settings["cod-sense"];
+    console.warn(JSON.stringify(config));
+    if(config.settings.active_lib != "" && config.settings.libs[config.settings.active_lib])
+    {
+        config.libraryPath = config.settings.libs[config.settings.active_lib];
+
+        try
         {
-            ast.analyzeWorkspace(files);
-        },
-        function(rejectReason) //Rejected
+            let stat = fs.lstatSync(config.libraryPath);
+            if(!stat.isDirectory())
+            {   
+                console.warn(`Config Key '${config.settings.active_lib}' has invalid directory path '${config.libraryPath}' - Ignoring cod-sense.active_lib`);
+                config.libraryPath = "";
+            }
+        }
+        catch(error)
         {
-            console.error("Rejected! - Couldn't get the workspace file list");
-            return;
-        });
+            console.error(error + " - Ignoring cod-sense.active_lib");
+            config.libraryPath = "";
+        }           
+    }
 }
