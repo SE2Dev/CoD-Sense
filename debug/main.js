04c1e416601e9f5a42ef74a3872498fbd9f75028
SE2Dev <brokensilencedev@gmail.com>
2016-03-21 15:24:59 -0400

    Add Performance Measurement Support

--- debug/main.js
+++ debug/main.js
@@ -5,8 +5,11 @@ var source = fs.readFileSync("file.gsc", 'utf8');
 var ast;
 
 try {
+    var startTime = new Date().getTime();
     ast = parser.parse(source);
+    var endTime = new Date().getTime();
     console.log(ast);
+    console.log(`PARSED: In ${endTime - startTime} ms`);
     //fs.writeFileSync("out.ast", JSON.stringify(ast), "utf8");
 }
 catch (exception) {
