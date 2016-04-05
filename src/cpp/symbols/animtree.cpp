4b7530f5bde2995d5960af0171a428a71f52f317
SE2Dev <brokensilencedev@gmail.com>
2016-04-05 13:18:08 -0400

    Move Excess Code from 'symbol.cpp'

--- src/cpp/symbols/animtree.cpp
+++ src/cpp/symbols/animtree.cpp
@@ -1,2 +1,31 @@
 #include "animtree.h"
 
+Animtree::Animtree(void) : string(NULL)
+{
+	this->type = S_TYPE_ANIMTREE;
+	//printf("%s\n", SYMBOL_TYPE_STRING(type));
+}
+
+Animtree::Animtree(String* animtree, YYLTYPE loc): string(animtree)
+{
+	this->type = S_TYPE_ANIMTREE;
+	this->location = loc;
+	//printf("%s animtree: '%s'\n", SYMBOL_TYPE_STRING(type), this->string->value);
+}
+
+Animtree::~Animtree()
+{
+	delete[] string;
+}
+
+void Animtree::PrintInfo()
+{
+	printf("%s with %d children at %d(%d) - %d(%d), str '%s'\n",
+		SYMBOL_TYPE_STRING(type),
+		this->children ? this->children->Size() + 1 : 0,
+		location.start.line,
+		location.start.character,
+		location.end.line,
+		location.end.character,
+		this->string->value);
+}
--- src/cpp/symbols/include.cpp
+++ src/cpp/symbols/include.cpp
@@ -1,2 +1,32 @@
 #include "include.h"
 
+
+Include::Include(void) : file(NULL)
+{
+	this->type = S_TYPE_INCLUDE;
+	//printf("%s\n", SYMBOL_TYPE_STRING(type));
+}
+
+Include::Include(String* filepath, YYLTYPE loc): file(filepath)
+{
+	this->type = S_TYPE_INCLUDE;
+	this->location = loc;
+	//printf("%s file: '%s'\n", SYMBOL_TYPE_STRING(type), this->file->value);
+}
+
+Include::~Include()
+{
+	delete[] file;
+}
+
+void Include::PrintInfo()
+{
+	printf("%s with %d children at %d(%d) - %d(%d), file '%s'\n",
+		SYMBOL_TYPE_STRING(type),
+		this->children ? this->children->Size() + 1 : 0,
+		location.start.line,
+		location.start.character,
+		location.end.line,
+		location.end.character,
+		this->file->value);
+}
