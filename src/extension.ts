8eb52f02044abbaf539010ba1caa09052f684627
SE2Dev <brokensilencedev@gmail.com>
2018-05-13 18:49:56 -0400

    Add Property Completion

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 'use strict';
 import * as vscode from 'vscode';
-import { completionItemProvider } from './features/completionItemProvider'
+import * as completion from './features/completionItemProvider'
 
 import {
 	ServerOptions, NodeModule, TransportKind,
@@ -16,7 +16,10 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log("CoD-Sense: Init");
 
 	// Register the built-in function definitions
-	vscode.languages.registerCompletionItemProvider("gsc", new completionItemProvider(context.extensionPath), "\\");
+	vscode.languages.registerCompletionItemProvider("gsc",
+		new completion.functionProvider(context.extensionPath));
+	vscode.languages.registerCompletionItemProvider("gsc",
+		new completion.propertyProvider(context.extensionPath), ".");
 }
 
 export function deactivate() {
--- src/features/completionItemProvider.ts
+++ src/features/completionItemProvider.ts
@@ -1,6 +1,8 @@
 'use strict';
 import * as vscode from 'vscode';
+
 import * as funcDefs from '../defs/defs'
+import * as fieldDefs from '../defs/field'
 
 import { server } from "../extension"
 import { CoDSenseResolveDirectoryRequest } from "../request"
@@ -10,11 +12,13 @@ import cp = require('child_process');
 
 var rxFilepath = /([_\w]+\\)+[_\w]*/;
 
-export class completionItemProvider {
-	completionItems: vscode.CompletionItem[];
+// Provides function completion
+export class functionProvider {
+	functions: vscode.CompletionItem[];
 
+	// Generate completion items for the hardcoded functions
 	constructor(extensionPath) {
-		this.completionItems = new Array<vscode.CompletionItem>();
+		this.functions = new Array<vscode.CompletionItem>();
 
 		for (var i in funcDefs.defs) {
 			var idef = funcDefs.defs[i];
@@ -23,7 +27,7 @@ export class completionItemProvider {
 			def.detail = idef.decl;
 			def.documentation = idef.desc;
 			def.kind = vscode.CompletionItemKind.Function;
-			this.completionItems.push(def);
+			this.functions.push(def);
 		}
 	}
 
@@ -33,19 +37,56 @@ export class completionItemProvider {
 		//
 		return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
 			// Dynamically resolved completion items
-			let completionItems: vscode.CompletionItem[] = [];
+			let funcItems: vscode.CompletionItem[] = [];
 
 			if (vscode.workspace.getConfiguration("cod-sense").get("use_builtin_completionItems", true)) {
 				// Fallback to just using the built-in completion items if no dynamic ones were found
-				if (!completionItems.length) {
-					resolve(this.completionItems);
+				if (!funcItems.length) {
+					resolve(this.functions);
 				}
 
-				resolve(this.completionItems.concat(completionItems));
+				resolve(this.functions.concat(funcItems));
 			}
 
 			// Use the non-builtin results (current none)
-			resolve(completionItems);
+			resolve(funcItems);
+		});
+	}
+}
+
+export class propertyProvider {
+	props: vscode.CompletionItem[];
+
+	// Generate completion items for the hardcoded functions
+	constructor(extensionPath) {
+		this.props = new Array<vscode.CompletionItem>();
+
+		for (var i in fieldDefs.fields) {
+			var def = new vscode.CompletionItem(fieldDefs.fields[i]);
+			def.kind = vscode.CompletionItemKind.Field;
+			this.props.push(def);
+		}
+	}
+
+	provideCompletionItems(document: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken,
+		context: vscode.CompletionContext): Thenable<vscode.CompletionItem[]> | vscode.CompletionItem[] {
+
+		// Don't provide the property completionItems unless we were activated by the trigger character
+		if (context.triggerKind != vscode.CompletionTriggerKind.TriggerCharacter)
+			return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
+				reject();
+			});
+
+		//
+		// Present the user with a list of common GSC / CSC functions
+		//
+		return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
+			// Dynamically resolved completion items
+			let propItems: vscode.CompletionItem[] = [];
+
+			resolve(this.props);
 		});
 	}
 }
